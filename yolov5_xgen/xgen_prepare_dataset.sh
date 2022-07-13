022559862297ee3661268703986d5483bc001a6c
tanqy <qiaoyu.tan@cocopie.ai>
2022-07-13 20:55:25 +0800

    [add]coco prepare

--- yolov5_xgen/xgen_prepare_dataset.sh
+++ yolov5_xgen/xgen_prepare_dataset.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-set -e
 data_path="/data/object-detection-yolov5"
+
+# 进入到数据存放目录并下载数据
 mkdir ${data_path} && cd ${data_path}
+wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/object-detection-yolov5/train2017.zip
+wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/object-detection-yolov5/val2017.zip
+wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/object-detection-yolov5/coco2017labels.zip
+wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/object-detection-yolov5/coco128.zip
+
+# 如果缓存目录不存在则进行数据准备
+cd ${data_path}
+if "folder1" and "folder2" 不存在：
+    解压 train2017.zip 
+    解压 val2017.zip 
+    解压 coco2017labels.zip
